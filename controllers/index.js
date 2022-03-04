81e772d77017eb52ff0128b53ffd279543befbf8
AhMeDMuEEd <mueed@usc.edu>
2022-03-04 11:00:59 -0800

    Changed back to handlebars
    Changed html back to handlebars and added lyricEditor to navbar

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,14 @@ const router = require('express').Router();
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
+const lyricRoutes = require('./lyric-routes.js');
+const { route } = require('./home-routes.js');
+
+
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+route.use('/lyricRoutes', lyricRoutes);
 
 module.exports = router;
